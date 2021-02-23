7f854585b7f176bd4feaa923ef6ed32000ddf7a6
Maximilian Schelbach <maximilian@schelbach.de>
2021-02-23 17:41:51 +0100

    simplify get_extension

--- FilePathV2.py
+++ FilePathV2.py
@@ -15,17 +15,11 @@ def get_filename(filepath: str):
     return filepath[filepath.rindex('/') + 1:]
 
 
-# Get the file extension of a file (e.g 'image.png' -> 'png')
-#  filepath -> the whole path to the file
-#  return   -> the extension of the file
-def get_extension_part(filepath):
-    try:
-        occurrences = [m.start() for m in re.finditer('\.', filepath)]
-        return filepath[occurrences[-1] + 1:]
-    except:
-        pass
+def get_extension(filepath: str):
+    if '.' not in filepath:
+        return ''
 
-    return ''
+    return filepath[filepath.rindex('.') + 1: ]
 
 
 assert (get_directory_name("log/cups/access_log") == "log/cups/")
@@ -36,6 +30,6 @@ assert (get_filename("log/cups/access_log") == "access_log")
 assert (get_filename("log/cups/") == "")
 assert (get_filename("cups/access_log") == "access_log")
 assert (get_filename("access_log") == "access_log")
-assert (get_extension_part("log/cups/access_log") == "")
-assert (get_extension_part("base/FileHelper.cpp") == "cpp")
-assert (get_extension_part("base/FileHelper.cpp.bak") == "bak")
+assert (get_extension("log/cups/access_log") == "")
+assert (get_extension("base/FileHelper.cpp") == "cpp")
+assert (get_extension("base/FileHelper.cpp.bak") == "bak")
