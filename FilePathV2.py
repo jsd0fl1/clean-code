a673767f3bc26ee91efe2f437e1a4d1249deb7ec
Maximilian Schelbach <maximilian@schelbach.de>
2021-02-23 17:40:58 +0100

    simplify get_filename

--- FilePathV2.py
+++ FilePathV2.py
@@ -1,23 +1,18 @@
 import re
 
+
 def get_directory_name(filepath: str):
     if '/' not in filepath:
         return ""
 
     return filepath[0: filepath.rindex('/') + 1]
 
-# Get the filename of a file path
-#  filepath -> the whole path to the file
-#  return   -> the filename without the path to the directory
-def get_filename_part(filepath):
-    try:
-        int(filepath.rindex('/'))
-    except:
+
+def get_filename(filepath: str):
+    if '/' not in filepath:
         return filepath
 
-    pos = filepath.rindex('/')
-    base_name = filepath[pos + 1:]
-    return base_name
+    return filepath[filepath.rindex('/') + 1:]
 
 
 # Get the file extension of a file (e.g 'image.png' -> 'png')
@@ -33,14 +28,14 @@ def get_extension_part(filepath):
     return ''
 
 
-assert(get_directory_name("log/cups/access_log") == "log/cups/")
-assert(get_directory_name("log/cups/") == "log/cups/")
-assert(get_directory_name("cups/access_log") == "cups/")
-assert(get_directory_name("access_log") == "")
-assert(get_filename_part("log/cups/access_log") == "access_log")
-assert(get_filename_part("log/cups/") == "")
-assert(get_filename_part("cups/access_log") == "access_log")
-assert(get_filename_part("access_log") == "access_log")
-assert(get_extension_part("log/cups/access_log") == "")
-assert(get_extension_part("base/FileHelper.cpp") == "cpp")
-assert(get_extension_part("base/FileHelper.cpp.bak") == "bak")
+assert (get_directory_name("log/cups/access_log") == "log/cups/")
+assert (get_directory_name("log/cups/") == "log/cups/")
+assert (get_directory_name("cups/access_log") == "cups/")
+assert (get_directory_name("access_log") == "")
+assert (get_filename("log/cups/access_log") == "access_log")
+assert (get_filename("log/cups/") == "")
+assert (get_filename("cups/access_log") == "access_log")
+assert (get_filename("access_log") == "access_log")
+assert (get_extension_part("log/cups/access_log") == "")
+assert (get_extension_part("base/FileHelper.cpp") == "cpp")
+assert (get_extension_part("base/FileHelper.cpp.bak") == "bak")
