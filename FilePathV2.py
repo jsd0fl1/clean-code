67423106e1c600e07719ad4e612a25e0c2777c80
Maximilian Schelbach <maximilian@schelbach.de>
2021-02-23 17:17:15 +0100

    fix get_filename_part naming

--- FilePathV2.py
+++ FilePathV2.py
@@ -21,7 +21,7 @@ def get_file(sFile):
     return dirName
 
 # This function gets the file
-def getFilenamePart(sFilename):
+def get_filename_part(sFilename):
     try:
         int(sFilename.rindex('/'))
     except:
@@ -47,10 +47,10 @@ assert(get_file("log/cups/access_log") == "log/cups/")
 assert(get_file("log/cups/") == "log/cups/")
 assert(get_file("cups/access_log") == "cups/")
 assert(get_file("access_log") == "")
-assert(getFilenamePart("log/cups/access_log") == "access_log")
-assert(getFilenamePart("log/cups/") == "")
-assert(getFilenamePart("cups/access_log") == "access_log")
-assert(getFilenamePart("access_log") == "access_log")
+assert(get_filename_part("log/cups/access_log") == "access_log")
+assert(get_filename_part("log/cups/") == "")
+assert(get_filename_part("cups/access_log") == "access_log")
+assert(get_filename_part("access_log") == "access_log")
 assert(get_extension_part("log/cups/access_log") == "")
 assert(get_extension_part("base/FileHelper.cpp") == "cpp")
-assert(get_extension_part("base/FileHelper.cpp.bak") == "bak") 
+assert(get_extension_part("base/FileHelper.cpp.bak") == "bak")
